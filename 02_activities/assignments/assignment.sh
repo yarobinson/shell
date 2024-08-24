4973903ca65681d02d5f9fe54d2d2b1004703297
Yasmine <yasminer0182@gmail.com>
2024-08-24 11:23:30 -0400

    completed questions 1 through 8 for shell assignment, ready for submission

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,13 +18,27 @@ mkdir dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+cd dir2
+pwd
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch file4.txt
+touch file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3
+echo "Hello World" > file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+cat file3.txt
 
 # 6. Delete file4
+rm -i file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
+cd ../
+rm -r dir4
+rm - r dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+ls
